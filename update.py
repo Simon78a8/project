e10ca0ccf1d445ef18bccece289cc279482a106b
Simon Ngugi <67118315+Simon78a8@users.noreply.github.com>
2022-05-23 10:21:49 +0300

    Update update.py

--- update.py
+++ update.py
@@ -5,23 +5,21 @@ Created on Thu Mar 31 13:14:25 2022
 @author: simon
 """
 
-from openpyxl import load_workbook
+from openpyxl import load_workbook  ##import workbook
 
-wb = load_workbook('produceSales.xlsx')
-sheet = wb.active
+wb = load_workbook('produceSales.xlsx')  #load workbook from file
+sheet = wb.active                        #activate sheet
 
 new_prices = [1.19, 3.08, 1.27]
 
 
-for row in sheet['B6:B8']:
+for row in sheet['B6:B8']:                            ##cells with prices to be updated
     for index, cell in enumerate(row):
         cell.value = new_prices[index]
         
         
         
-    #for cell in row:
-        #print(cell.value)
-    #print(row)
+    
     
     
 wb.save('new_produceSales.xlsx')
